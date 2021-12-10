8825ab839a8ad1a7bf28bc38c9b59cfaeb77ff39
I Made Juniandika <74083091+juniade@users.noreply.github.com>
2021-12-10 18:29:37 +0800

    Update Dictionary.py
    menmabhkan menu login dan register

--- Dictionary.py
+++ Dictionary.py
@@ -86,20 +86,92 @@ def interactiveMode(hash):
             print(f"[blink bold red] Incorrect Input!![/blink bold red]")
 
         system.pause()
+def register():
+    db = open("database.txt", "r")
+    username = input("Masukkan username: ")
+    password = input("Masukkan password: ")
+    confirm = input("Konfirmasi password: ")
+    username1 = []
+    password1 = []
+    for i in db:
+        a, b = i.split(",")
+        b = b.strip()
+        username1.append(a)
+        password1.append(b)
+    data = dict(zip(username1, password1))
+    print(data)
+    if password != confirm:
+        print("Password tidak sesuai")
+        register()
+    else:
+        if len(password) <= 6:
+            print("Password terlalu pendek")
+            register()
+        elif username in username1:
+            print("Akun sudah terdaftar")
+            register()
+        else:
+            db = open("database.txt", "a")
+            db.write(username + "," + password + "\n")
+            print('Success!')
+            access()
+
+
+def access():
+    db = open("database.txt", "r")
+
+    username = input("Masukkan username: ")
+    password = input("Masukkan password: ")
+
+    if not len(username or password) < 1:
+        username1 = []
+        password1 = []
+        for i in db:
+            a, b = i.split(",")
+            b = b.strip()
+            username1.append(a)
+            password1.append(b)
+        data = dict(zip(username1, password1))
+        try:
+            if data[username]:
+                try:
+                    if password == data[username]:
+                        print("Login Success")
+                        print("Hai, ", username)
+                        if not os.path.exists(f'DB/'):
+                            os.mkdir("DB/")
+                            loadDB(defaultDB)
+
+                        hash = Hash()
+                        loadTable(hash)
+
+                        if len(sys.argv) == 1:
+                            interactiveMode(hash)
+                        else:
+                            commandLineMode(hash, sys.argv[1])
+
+                    else:
+                        print("Password atau Username anda salah")
+                except:
+                    print("Terjadi kesalahan")
+            else:
+                print("username tidak terdaftar")
+        except:
+            print("Login Eror")
+
+
+def home(option=None):
+    option = input("Login | Signup: ")
+    if option == 'Login':
+        access()
+    elif option == 'Signup':
+        register()
+    else:
+        print("please, enter an option")
+
 
 def main():
-    if not os.path.exists(f'DB/'):
-        os.mkdir("DB/")
-        loadDB(defaultDB)
-
-    hash = Hash()
-    loadTable(hash)
-
-    if len(sys.argv) == 1:
-        interactiveMode(hash)
-    else:
-        commandLineMode(hash, sys.argv[1])
-
+    home()
 
 if __name__ == '__main__':
     main()
